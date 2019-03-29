6ec384172db1c6ae52341fd5f605b4eda4287582
narumoto65 <narumoto65@gmail.com>
2019-03-29 19:48:09 +0900

    side2

--- screens/Home/ProfileContainer.js
+++ screens/Home/ProfileContainer.js
@@ -14,17 +14,52 @@ import {createStore,applyMiddleware} from 'redux';
 import {Provider,connect} from 'react-redux';
 import thunk from 'redux-thunk';
 import _ from 'lodash';
+import {createStackNavigator,
+        createAppContainer} from 'react-navigation';
 import {readEvents} from '../../actions';
 import reducer from '../../reducers';
 import Profile from './Profile';
+import Calendars from './Calendars';
+import Confirm from './Confirm';
+import Monsi from './Monsi';
 export const store = createStore(reducer,applyMiddleware(thunk))
 
+const RootStack = createStackNavigator({
+    Profile:{
+        screen:Profile,
+        navigationOptions:()=>({
+            header:null
+        })
+    },
+    Calendars:{
+        screen:Calendars,
+        navigationOptions:()=>({
+            header:null
+        })
+    },
+    Confirm:{
+        screen:Confirm,
+        navigationOptions:()=>({
+            header:null
+        })
+    },
+    Monsi:{
+        screen:Monsi,
+        navigationOptions:()=>({
+            header:null
+        })
+    }
+},{
+    initialRouteName:'Profile'
+})
+
+let Navigation = createAppContainer(RootStack);
 class ProfileContainer extends Component {
     render() {
         return (
             <Container>
                 <Provider store={store}>
-                    <Profile/>
+                    <Navigation/>
                 </Provider>
             </Container>
         );
